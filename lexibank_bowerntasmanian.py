749a8163c42dde33f0d1d73d06b00ee69f2c21f9
SimonGreenhill <simon@simon.net.nz>
2019-12-18 22:34:01 +0100

    working

--- lexibank_bowerntasmanian.py
+++ lexibank_bowerntasmanian.py
@@ -1,26 +1,70 @@
-import pathlib
+from pathlib import Path
 
-from cldfbench import Dataset as BaseDataset
+import attr
+from clldutils.misc import slug
+from pylexibank import Dataset as BaseDataset
+from pylexibank import Language as BaseLanguage
+from pylexibank import FormSpec
+
+@attr.s
+class CustomLanguage(BaseLanguage):
+    # the tokens used as language identifiers by Robinson and Holton
+    RegionCode = attr.ib(default=None)
+    Region = attr.ib(default=None)
+    MacroRegion = attr.ib(default=None)
+
 
 
 class Dataset(BaseDataset):
-    dir = pathlib.Path(__file__).parent
+    dir = Path(__file__).parent
     id = "bowerntasmanian"
+    language_class = CustomLanguage
 
-    def cldf_specs(self):  # A dataset must declare all CLDF sets it creates.
-        return super().cldf_specs()
-
-    def cmd_download(self, args):
-        """
-        Download files to the raw/ directory. You can use helpers methods of `self.raw_dir`, e.g.
-
-        >>> self.raw_dir.download(url, fname)
-        """
-        pass
-
+    form_spec = FormSpec(
+        brackets={"[": "]", "{": "}", "(": ")", "‘": "’"},
+        separators=";/,",
+        missing_data=("?", "-", "--"),
+        strip_inside_brackets=True,
+    )
+    
     def cmd_makecldf(self, args):
         """
         Convert the raw data to a CLDF dataset.
-
-        >>> args.writer.objects['LanguageTable'].append(...)
         """
+        args.writer.add_sources()
+        
+        languages = args.writer.add_languages(lookup_factory="Name")
+        
+        concepts = args.writer.add_concepts(
+            id_factory=lambda c: c.id.split("-")[-1] + "_" + slug(c.english),
+            lookup_factory="Name"
+        )
+        # manually patch a few concepts because of « (in concepticon) = " in data
+        concepts['blue-tongued lizard ("goanna")'] = '49_bluetonguedlizardgoanna'
+        concepts['trunk ("tree")'] = '577_trunktree'
+        
+        for i, row in enumerate(self.raw_dir.read_csv('data.tsv', dicts=True, delimiter="\t"), 1):
+            # skip rows with undefined master glosses as these seem to be
+            # unusual/unidentified forms.
+            if not row['MasterGloss']:
+                continue
+            
+            # skip these unknown languages
+            if row['region'] in ("", "unknown15"):
+                continue
+            
+            lang = languages.get(row['region'])
+            print(lang)
+            if lang is None:
+                print('Unknown language %d: %s' % (i, row['region']))
+                continue
+            
+            args.writer.add_forms_from_value(
+                Local_ID=row['recordid'],
+                Language_ID=lang,
+                Parameter_ID=concepts[row['MasterGloss']],
+                Value=row['originalform'],
+                Source=["Bowern2012"],
+            )
+
+            quit()
--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 from setuptools import setup
+import json
+
+
+with open("metadata.json") as fp:
+    metadata = json.load(fp)
 
 
 setup(
-    name='cldfbench_bowerntasmanian',
-    py_modules=['cldfbench_bowerntasmanian'],
+    name="lexibank_bowerntasmanian",
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_bowerntasmanian"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'cldfbench.dataset': [
-            'bowerntasmanian=cldfbench_bowerntasmanian:Dataset',
-        ]
-    },
-    install_requires=[
-        'cldfbench',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
+    entry_points={"lexibank.dataset": ["bowerntasmanian=lexibank_bowerntasmanian:Dataset"]},
+    install_requires=["pylexibank>=2.1"],
+    extras_require={"test": ["pytest-cldf"]},
 )
